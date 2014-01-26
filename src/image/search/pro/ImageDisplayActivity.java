5e4660615c7ac8e07b3dc06d9f6e5af5fa40500e
Mike Huff <mike@grio.com>
2014-01-26 15:28:45 -0800

    add project

---
+++ src/image/search/pro/ImageDisplayActivity.java
@@ -0,0 +1,30 @@
+package image.search.pro;
+
+import com.loopj.android.image.SmartImageView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ImageDisplayActivity extends Activity {
+
+	SmartImageView ivImage;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_image_display);
+		ImageResult image = (ImageResult) getIntent().getSerializableExtra("result");
+		ivImage = (SmartImageView) findViewById(R.id.ivResult);
+		ivImage.setImageUrl(image.getFullUrl());
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.image_display, menu);
+		return true;
+	}
+
+}
